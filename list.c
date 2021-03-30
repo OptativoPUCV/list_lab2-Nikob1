08ab97aa0a21cc50c29f0310bcfbc1f4a77832bc
Replit user <replituser@example.com>
2021-03-30 20:30:40 +0000

    new try

--- list.c
+++ list.c
@@ -109,7 +109,8 @@ void pushCurrent(List * list, const void * data) {
         aux=list->current;
         list->current->next=nodo;
         nodo->prev=aux;
-        
+        aux->next->prev=nodo;
+        nodo->next=aux->next;
       }
 
     }
